869fd435ecf05a53c5b83eef5a8fad685e1dd4da
Seppe Snoeck <106682934+seppesnoeck@users.noreply.github.co>
2022-10-17 15:50:23 +0200

    Update documentation for Binding extensions

--- Sources/Nodes/SwiftUI/Binding.swift
+++ Sources/Nodes/SwiftUI/Binding.swift
@@ -21,7 +21,8 @@ extension Binding {
     ///         Slider(value: .binding(to: viewState.sliderValue, onChange: receiver?.didChangeSliderValue), in: 1...100)
     ///     }
     /// }
-    /// ```
+    /// ```AbstractPresentableContext
+
     ///
     /// - Parameters:
     ///   - value: A value for the getter of the binding.
@@ -32,11 +33,24 @@ extension Binding {
         Binding(get: { value }, set: { onChange($0) })
     }
 
-    /// Create a Binding for a value with an optional callback listening for a change for the passed value.
+    /// A Binding factory to be used instead of the built-in SwiftUI provided initializer for two reasons:
+    /// - Accepts a value for the getter instead of a closure
+    /// - Allows for `.binding` which is more declarative than `.init`
+    ///
+    /// Example:
+    /// ```
+    /// var body: some View {
+    ///     WithViewState(viewState) { viewState in
+    ///         Slider(value: .binding(to: viewState.sliderValue, onChange: receiver?.didChangeSliderValue), in: 1...100)
+    ///     }
+    /// }
+    /// ```
+    ///
     /// - Parameters:
-    ///   - value: The value you want to create a Binding for.
-    ///   - onChange: The optional callback listinging for a change of the passed value
-    /// - Returns: Returns a SwiftUI Binding for the passed in value
+    ///   - value: A value for the getter of the binding.
+    ///   - onChange: An optional closure for the setter of the binding.
+    ///
+    /// - Returns: A SwiftUI Binding instance.
     public static func binding(to value: Value, onChange: ((Value) -> Void)?) -> Binding<Value> {
         guard let onChange: (Value) -> Void = onChange
         else { return .constant(value) }
