ea3d2710faea14813310214904ce589524157df4
Seppe Snoeck <106682934+seppesnoeck@users.noreply.github.com>
2022-10-31 10:18:04 +0100

    Test multiple types for both factories
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Tests/NodesTests/SwiftUI/BindingTests.swift
+++ Tests/NodesTests/SwiftUI/BindingTests.swift
@@ -13,10 +13,15 @@ import XCTest
 @available(macOS 10.15, macCatalyst 13.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 final class BindingTests: XCTestCase {
 
-    func testBinding() {
-        expect(Binding.binding(to: 1) { _ in }).to(beAKindOf(Binding<Int>.self))
-        expect(Binding.binding(to: 1, onChange: nil)).to(beAKindOf(Binding<Int>.self))
-        expect(Binding.binding(to: "Tinder", onChange: nil)).notTo(beAKindOf(Binding<Int>.self))
+    func testBindingFactory() {
+        expect(Binding.binding(to: true) { _ in }).to(beAKindOf(Binding<Bool>.self))
+        expect(Binding.binding(to: 23) { _ in }).to(beAKindOf(Binding<Int>.self))
+        expect(Binding.binding(to: "Hello World") { _ in }).to(beAKindOf(Binding<String>.self))
+    }
+
+    func testBindingFactoryWithOptionalSetter() {
         expect(Binding.binding(to: true, onChange: nil)).to(beAKindOf(Binding<Bool>.self))
+        expect(Binding.binding(to: 23, onChange: nil)).to(beAKindOf(Binding<Int>.self))
+        expect(Binding.binding(to: "Hello World", onChange: nil)).to(beAKindOf(Binding<String>.self))
     }
 }
