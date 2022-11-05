dd6dd13a7f969259c4ebfa32ff53980e0988cbe6
Garric Nahapetian <garric.nahapetian@gotinder.com>
2022-11-04 17:17:06 -0700

    Snapshot allCases

--- Tests/XcodeTemplateGeneratorLibraryTests/StencilTemplateTests.swift
+++ Tests/XcodeTemplateGeneratorLibraryTests/StencilTemplateTests.swift
@@ -5,21 +5,17 @@
 //  Created by Garric Nahapetian on 11/4/22.
 //
 
+import SnapshotTesting
 import XcodeTemplateGeneratorLibrary
 import XCTest
 
 final class StencilTemplateTests: XCTestCase {
 
     func testRawValue() {
-        XCTAssertEqual(StencilTemplate.analytics.rawValue, "Analytics")
-        XCTAssertEqual(StencilTemplate.builder.rawValue, "Builder")
-        XCTAssertEqual(StencilTemplate.builderSwiftUI.rawValue, "Builder-SwiftUI")
-        XCTAssertEqual(StencilTemplate.context.rawValue, "Context")
-        XCTAssertEqual(StencilTemplate.flow.rawValue, "Flow")
-        XCTAssertEqual(StencilTemplate.plugin.rawValue, "Plugin")
-        XCTAssertEqual(StencilTemplate.pluginList.rawValue, "PluginList")
-        XCTAssertEqual(StencilTemplate.viewController.rawValue, "ViewController")
-        XCTAssertEqual(StencilTemplate.viewControllerSwiftUI.rawValue, "ViewController-SwiftUI")
-        XCTAssertEqual(StencilTemplate.worker.rawValue, "Worker")
+        assertSnapshot(matching: StencilTemplate.allCases.map(\.rawValue), as: .dump)
+    }
+
+    func testAllCases() {
+        assertSnapshot(matching: StencilTemplate.allCases, as: .dump)
     }
 }
