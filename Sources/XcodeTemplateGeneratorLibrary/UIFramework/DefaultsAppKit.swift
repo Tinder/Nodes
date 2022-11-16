54d65e88c727e68a627f661ca0b38a09833c7a18
Garric Nahapetian <garric.nahapetian@gotinder.com>
2022-11-16 15:26:34 -0800

    Update DefaultsAppKit

--- Sources/XcodeTemplateGeneratorLibrary/UIFramework/DefaultsAppKit.swift
+++ Sources/XcodeTemplateGeneratorLibrary/UIFramework/DefaultsAppKit.swift
@@ -9,10 +9,45 @@ extension UIFramework {
 
     internal final class DefaultsAppKit {
 
-        private let viewControllerSuperParameters: String = ""
+        private let viewControllerSuperParameters: String = "nibName: nil, bundle: nil"
         private let viewControllerProperties: String = ""
-        private let viewControllerMethods: String = ""
-        private let viewControllerMethodsForRootNode: String = ""
+
+        private let viewControllerMethods: String = """
+            override func viewDidLoad() {
+                super.viewDidLoad()
+            }
+
+            override func viewWillAppear() {
+                super.viewWillAppear()
+                observe(viewState).store(in: &cancellables)
+            }
+
+            override func viewWillDisappear() {
+                super.viewWillDisappear()
+                cancellables.removeAll()
+            }
+            """
+
+        private let viewControllerMethodsForRootNode: String = """
+            override func viewDidLoad() {
+                super.viewDidLoad()
+            }
+
+            override func viewWillAppear() {
+                super.viewWillAppear()
+                observe(viewState).store(in: &cancellables)
+            }
+
+            override func viewDidAppear() {
+                super.viewDidAppear()
+                receiver?.viewDidAppear()
+            }
+
+            override func viewWillDisappear() {
+                super.viewWillDisappear()
+                cancellables.removeAll()
+            }
+            """
 
         internal func makeUIFramework() -> UIFramework {
             UIFramework(
