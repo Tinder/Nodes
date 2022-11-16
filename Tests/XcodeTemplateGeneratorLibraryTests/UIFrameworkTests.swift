e4a82e03d28299cf25a2932fef5fdd3cbc1e3830
Garric Nahapetian <garric.nahapetian@gotinder.com>
2022-11-15 19:46:56 -0800

    Use expect instead of snapshot in UIFrameworkTests

--- Tests/XcodeTemplateGeneratorLibraryTests/UIFrameworkTests.swift
+++ Tests/XcodeTemplateGeneratorLibraryTests/UIFrameworkTests.swift
@@ -5,6 +5,7 @@
 //  Created by Garric Nahapetian on 11/14/22.
 //
 
+import Nimble
 import SnapshotTesting
 @testable import XcodeTemplateGeneratorLibrary
 import XCTest
@@ -12,18 +13,75 @@ import Yams
 
 internal final class UIFrameworkTests: XCTestCase {
 
-    internal func testInitWithFramework() {
-        let frameworks: [UIFramework.Framework] = [
-            .appKit,
-            .uiKit,
-            .swiftUI,
-            .custom(name: "Custom", import: "<import>", viewControllerType: "<viewControllerType>")
-        ]
-        frameworks
-            .map(UIFramework.init)
-            .forEach {
-                assertSnapshot(matching: $0, as: .dump, named: $0.name)
-            }
+    internal func testInitWithAppKit() {
+        let defaults: UIFramework = .DefaultsAppKit().makeUIFramework()
+        let appKit: UIFramework = .init(framework: .appKit)
+        expect(appKit.kind) == defaults.kind
+        expect(appKit.name) == defaults.name
+        expect(appKit.import) == defaults.import
+        expect(appKit.viewControllerType) == defaults.viewControllerType
+        expect(appKit.viewControllerSuperParameters) == defaults.viewControllerSuperParameters
+        expect(appKit.viewControllerProperties) == defaults.viewControllerProperties
+        expect(appKit.viewControllerMethods) == defaults.viewControllerMethods
+        expect(appKit.viewControllerMethodsForRootNode) == defaults.viewControllerMethodsForRootNode
+    }
+
+    internal func testInitWithUIKit() {
+        let defaults: UIFramework = .DefaultsUIKit().makeUIFramework()
+        let uiKit: UIFramework = .init(framework: .uiKit)
+        expect(uiKit.kind) == defaults.kind
+        expect(uiKit.name) == defaults.name
+        expect(uiKit.import) == defaults.import
+        expect(uiKit.viewControllerType) == defaults.viewControllerType
+        expect(uiKit.viewControllerSuperParameters) == defaults.viewControllerSuperParameters
+        expect(uiKit.viewControllerProperties) == defaults.viewControllerProperties
+        expect(uiKit.viewControllerMethods) == defaults.viewControllerMethods
+        expect(uiKit.viewControllerMethodsForRootNode) == defaults.viewControllerMethodsForRootNode
+    }
+
+    internal func testInitWithSwiftUI() {
+        let defaults: UIFramework = .DefaultsSwiftUI().makeUIFramework()
+        let swiftUI: UIFramework = .init(framework: .swiftUI)
+        expect(swiftUI.kind) == defaults.kind
+        expect(swiftUI.name) == defaults.name
+        expect(swiftUI.import) == defaults.import
+        expect(swiftUI.viewControllerType) == defaults.viewControllerType
+        expect(swiftUI.viewControllerSuperParameters) == defaults.viewControllerSuperParameters
+        expect(swiftUI.viewControllerProperties) == defaults.viewControllerProperties
+        expect(swiftUI.viewControllerMethods) == defaults.viewControllerMethods
+        expect(swiftUI.viewControllerMethodsForRootNode) == defaults.viewControllerMethodsForRootNode
+    }
+
+    internal func testInitWithCustom() {
+        let framework: UIFramework.Framework = .custom(
+            name: "<name>", import: "<import>", viewControllerType: "<viewControllerType>"
+        )
+        let defaults: UIFramework = .Defaults().makeUIFramework(for: framework)
+        let custom: UIFramework = .init(framework: framework)
+        expect(custom.kind) == defaults.kind
+        expect(custom.name) == "<name>"
+        expect(custom.import) == "<import>"
+        expect(custom.viewControllerType) == defaults.viewControllerType
+        expect(custom.viewControllerSuperParameters) == defaults.viewControllerSuperParameters
+        expect(custom.viewControllerProperties) == defaults.viewControllerProperties
+        expect(custom.viewControllerMethods) == defaults.viewControllerMethods
+        expect(custom.viewControllerMethodsForRootNode) == defaults.viewControllerMethodsForRootNode
+    }
+
+    internal func testInitWithCustomDefaults() {
+        let framework: UIFramework.Framework = .custom(
+            name: nil, import: nil, viewControllerType: "<viewControllerType>"
+        )
+        let defaults: UIFramework = .Defaults().makeUIFramework(for: framework)
+        let custom: UIFramework = .init(framework: framework)
+        expect(custom.kind) == defaults.kind
+        expect(custom.name) == defaults.name
+        expect(custom.import).to(beNil())
+        expect(custom.viewControllerType) == "<viewControllerType>"
+        expect(custom.viewControllerSuperParameters) == defaults.viewControllerSuperParameters
+        expect(custom.viewControllerProperties) == defaults.viewControllerProperties
+        expect(custom.viewControllerMethods) == defaults.viewControllerMethods
+        expect(custom.viewControllerMethodsForRootNode) == defaults.viewControllerMethodsForRootNode
     }
 
     internal func testInitFromDecoderWithYaml() throws {
@@ -34,28 +92,12 @@ internal final class UIFrameworkTests: XCTestCase {
         }
     }
 
-    internal func testInitFromDecoderWithDefaults() throws {
+    internal func testInitFromDecoderWithYamlUsingDefaults() throws {
         try UIFramework.Kind.allCases.forEach {
             let data: Data = .init(givenEmptyUIFrameworkYaml(for: $0).utf8)
             let uiFramework: UIFramework = try YAMLDecoder().decode(UIFramework.self, from: data)
             assertSnapshot(matching: uiFramework, as: .dump, named: $0.rawValue)
         }
-    }
-
-    internal func testComputedProperties() {
-        let frameworks: [UIFramework.Framework] = [
-            .appKit,
-            .uiKit,
-            .swiftUI,
-            .custom(name: "Custom", import: "<import>", viewControllerType: "<viewControllerType>")
-        ]
-        frameworks
-            .map(UIFramework.init)
-            .forEach {
-                assertSnapshot(
-                    matching: ($0.kind, $0.name, $0.import, $0.viewControllerType), as: .dump, named: $0.name
-                )
-            }
     }
 
     private func givenUIFrameworkYaml(for kind: UIFramework.Kind) -> String {
