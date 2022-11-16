8cfd901e2f4a7e29752d9190e104a77cbbca9487
Garric Nahapetian <garric.nahapetian@gotinder.com>
2022-11-15 19:49:26 -0800

    Use expect in UIFrameworkKindTests

--- Tests/XcodeTemplateGeneratorLibraryTests/UIFrameworkKindTests.swift
+++ Tests/XcodeTemplateGeneratorLibraryTests/UIFrameworkKindTests.swift
@@ -5,17 +5,17 @@
 //  Created by Garric Nahapetian on 11/14/22.
 //
 
-import SnapshotTesting
+import Nimble
 @testable import XcodeTemplateGeneratorLibrary
 import XCTest
 
 internal final class UIFrameworkKindTests: XCTestCase {
 
     internal func testAllCases() {
-        assertSnapshot(matching: UIFramework.Kind.allCases, as: .dump)
+        expect(UIFramework.Kind.allCases) == [.appKit, .uiKit, .swiftUI, .custom]
     }
 
     internal func testRawValues() {
-        assertSnapshot(matching: UIFramework.Kind.allCases.map(\.rawValue), as: .dump)
+        expect(UIFramework.Kind.allCases.map(\.rawValue)) == ["AppKit", "UIKit", "SwiftUI", "Custom"]
     }
 }
