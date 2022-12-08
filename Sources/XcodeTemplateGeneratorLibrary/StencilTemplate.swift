7733a54ebd41b3ca87c185ca44283b9d88af6d37
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2022-12-08 10:22:13 -0800

    Fix typo

--- Sources/XcodeTemplateGeneratorLibrary/StencilTemplate.swift
+++ Sources/XcodeTemplateGeneratorLibrary/StencilTemplate.swift
@@ -66,7 +66,7 @@ public enum StencilTemplate: Equatable, CaseIterable, CustomStringConvertible {
         }
     }
 
-    /// The Stencil source file name in the XcodeTemplateGeneratorLibrary bundle.
+    /// The Stencil source filename in the XcodeTemplateGeneratorLibrary bundle.
     public var filename: String {
         switch self {
         case .analytics, .context, .flow, .plugin, .pluginList, .worker:
