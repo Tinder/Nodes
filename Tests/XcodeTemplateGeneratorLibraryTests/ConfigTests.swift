f902f34abedee6c5d7049414b786f68616f1cbc1
Garric Nahapetian <garric.nahapetian@gotinder.com>
2022-12-12 17:32:04 -0800

    Disambiguate mock data with “uiFramework”  prefix

--- Tests/XcodeTemplateGeneratorLibraryTests/ConfigTests.swift
+++ Tests/XcodeTemplateGeneratorLibraryTests/ConfigTests.swift
@@ -41,8 +41,8 @@ final class ConfigTests: XCTestCase {
             UIFramework(framework: .appKit),
             UIFramework(framework: .uiKit),
             UIFramework(framework: .swiftUI),
-            UIFramework(framework: .custom(name: "<name>",
-                                           import: "<import>",
+            UIFramework(framework: .custom(name: "<uiFrameworkName>",
+                                           import: "<uiFrameworkImport>",
                                            viewControllerType: "<viewControllerType>",
                                            viewControllerSuperParameters: "<viewControllerSuperParameters>")
             )
@@ -82,8 +82,8 @@ final class ConfigTests: XCTestCase {
               viewControllerMethodsForRootNode: <viewControllerMethodsForRootNode-SwiftUI>
             - framework:
                 custom:
-                  name: <name>
-                  import: <import>
+                  name: <uiFrameworkName>
+                  import: <uiFrameworkImport>
                   viewControllerType: <viewControllerType>
                   viewControllerSuperParameters: <viewControllerSuperParameters>
               viewControllerProperties: <viewControllerProperties-Custom>
