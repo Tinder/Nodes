53e85ac83d2dfca00650bd40625cd5871b8b7723
Eman Haroutunian <104394983+tinder-emanharoutunian@users.noreply.github.com>
2023-01-12 12:56:10 -0800

    Update Package.swift
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Package.swift
+++ Package.swift
@@ -94,8 +94,7 @@ let package = Package(
                 "NodesTesting",
                 "Nimble",
                 .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
-            ],
-            exclude: ["__Snapshots__"]),
+            ]),
         .testTarget(
             name: "XcodeTemplateGeneratorLibraryTests",
             dependencies: [
