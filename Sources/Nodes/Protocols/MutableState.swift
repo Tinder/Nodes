beab4e582b1259cfb221d41f7eeff1b362c8a619
Seppe Snoeck <106682934+seppesnoeck@users.noreply.github.com>
2023-03-09 14:00:26 +0100

    Remove unneeded @available
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Sources/Nodes/Protocols/MutableState.swift
+++ Sources/Nodes/Protocols/MutableState.swift
@@ -12,7 +12,6 @@ public protocol MutableState {
     func with(_ mutation: (inout Self) throws -> Void) rethrows -> Self
 }
 
-@available(macOS 10.15, macCatalyst 13.0, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 extension MutableState {
 
     public mutating func apply(_ mutation: (inout Self) throws -> Void) rethrows {
