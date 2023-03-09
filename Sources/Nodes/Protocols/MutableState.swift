abb6ee0dc75f12163c25698f8ac9baef757b1de9
Seppe Snoeck <106682934+seppesnoeck@users.noreply.github.co>
2023-03-09 14:38:07 +0100

    Add DocC to MutableState methods

--- Sources/Nodes/Protocols/MutableState.swift
+++ Sources/Nodes/Protocols/MutableState.swift
@@ -7,8 +7,35 @@
 
 public protocol MutableState {
 
+    /// Mutate an instance with a new value.
+    /// - Parameter mutation: The mutation itself
+    ///
+    /// Example:
+    /// Send a new value on a CurrentValueSubject by mutating the current value:
+    /// ```
+    /// let subject: CurrentValueSubject<Example, Never> = .init(Example())
+    /// subject.mutate { $0.exampleProperty = 23 }
+    /// ```
+    ///
+    /// Mutate a struct variable:
+    /// ```
+    /// var example: Example = .init()
+    /// example.mutate { $0.exampleProperty = 23 }
+    /// ```
     mutating func apply(_ mutation: (inout Self) throws -> Void) rethrows
 
+    /// Create a new instance with the wanted changes.
+    /// - Parameter mutation: The mutation itself
+    ///
+    /// Example:
+    /// Mutate a struct constant:
+    /// ```
+    /// let publisher: AnyPublisher<Example, Never> = ...
+    ///
+    /// publisher.map { example in
+    ///    example.with { $0.exampleProperty = 23 }
+    /// }
+    /// ```
     func with(_ mutation: (inout Self) throws -> Void) rethrows -> Self
 }
 
