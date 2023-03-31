df5650d11a50601d8bc6c6cdd035a94ce62977ef
Garric Nahapetian <31713341+tinder-garricnahapetian@users.noreply.github.com>
2023-03-30 17:26:34 -0700

    Improve documentation comments
    Co-authored-by: Christopher Fuller <christopher.fuller@gotinder.com>

--- Sources/Nodes/Protocols/MutableState.swift
+++ Sources/Nodes/Protocols/MutableState.swift
@@ -7,7 +7,7 @@
 
 /**
  * ``MutableState`` specifies a method to apply changes to an instance and a method for creating a new
- * instance by applying changes to the existing instance. Protocol extension methods are defined and
+ * instance by applying changes to the receiver. Protocol extension methods are defined and
  * provide default implementations.
  *
  * ``MutableState`` may also be used with [Combine](https://developer.apple.com/documentation/combine) in
@@ -63,7 +63,7 @@ public protocol MutableState {
     /// ```
     mutating func apply(_ changes: (inout Self) throws -> Void) rethrows
 
-    /// Creates a new instance by applying changes to the existing instance.
+    /// Creates a new instance by applying changes to the receiver.
     ///
     /// - Parameter changes: The closure in which properties are mutated.
     ///
