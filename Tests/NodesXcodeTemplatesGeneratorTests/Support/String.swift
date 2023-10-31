f1855b4f9c96aeebe04a803ffb000bb524937099
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-10-31 15:37:22 -0700

    Shorten snapshot filenames

---
+++ Tests/NodesXcodeTemplatesGeneratorTests/Support/String.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2023 Tinder (Match Group, LLC)
+//
+
+import Foundation
+
+extension String {
+
+    public func replacingOccurrences(
+        of targets: [String],
+        with replacement: String
+    ) -> String {
+        targets.reduce(self) { partialResult, target in
+            partialResult.replacingOccurrences(of: target, with: replacement)
+        }
+    }
+}
