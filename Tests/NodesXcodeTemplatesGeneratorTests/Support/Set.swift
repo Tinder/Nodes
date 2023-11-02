38cf9def7a19d3aba7f3d62ef5059fb176097141
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-11-02 14:14:36 -0700

    Add contextGenericTypes and workerGenericTypes

--- Tests/NodesXcodeTemplatesGeneratorTests/Support/Set.swift
+++ Tests/NodesXcodeTemplatesGeneratorTests/Support/Set.swift
@@ -5,11 +5,6 @@
 extension Set where Element == String {
 
     internal static func mock(with identifier: String, count: Int) -> Self {
-        guard count > 0
-        else { return [] }
-        guard count > 1
-        else { return ["<\(identifier)>"] }
-        let strings: [String] = (1...count).map { "<\(identifier)\($0)>" }
-        return Set(strings)
+        Set([String].mock(with: identifier, count: count))
     }
 }
