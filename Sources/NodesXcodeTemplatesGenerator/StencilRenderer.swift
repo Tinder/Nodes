f264bc5b71e29a31fc34e0c54afe4dfc4714c881
Garric Nahapetian <garric.nahapetian@gotinder.com>
2023-12-01 15:09:34 -0800

    Rename stencilExtension

--- Sources/NodesXcodeTemplatesGenerator/StencilRenderer.swift
+++ Sources/NodesXcodeTemplatesGenerator/StencilRenderer.swift
@@ -75,12 +75,12 @@ public final class StencilRenderer {
     }
 
     private func extensions() -> [Extension] {
-        let ext: Extension = .init()
-        ext.registerFilter("decapitalize") { value in
+        let stencilExtension: Extension = .init()
+        stencilExtension.registerFilter("decapitalize") { value in
             guard let string = value as? String
             else { return value }
             return string.prefix(1).lowercased() + string.dropFirst()
         }
-        return [ext]
+        return [stencilExtension]
     }
 }
