433aea95447c2458e490824a12f26baaf9f4afa6
Christopher Fuller <christopher.fuller@gotinder.com>
2023-12-22 09:07:07 -0800

    Use initializer instead of string literal

--- Tests/NodesXcodeTemplatesGeneratorTests/Support/AssertSnapshot.swift
+++ Tests/NodesXcodeTemplatesGeneratorTests/Support/AssertSnapshot.swift
@@ -31,7 +31,7 @@ internal func assertSnapshot<Value, Format>(
         record: recording,
         snapshotDirectory: snapshotDirectory.path,
         timeout: timeout,
-        file: "",
+        file: StaticString(), // <- Intentionally avoiding potential (invalid relative path) magic file name use.
         testName: testName,
         line: line
     )
