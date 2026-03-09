b8b561ddc9c3b81a3ec9f864b99588c8d75d8b12
Christopher Fuller <christopher.fuller@gotinder.com>
2026-03-09 11:25:49 -0700

    Apply standard Swift package configuration (#920)
    * Apply standard Swift package configuration
    
    * Update genesis template
    
    * Fix

--- Package.swift
+++ Package.swift
@@ -53,7 +53,7 @@ let package = Package(
             url: "https://github.com/uber/needle.git",
             exact: "0.25.1"),
         .package(
-            url: "https://github.com/realm/SwiftLint.git",
+            url: "https://github.com/SimplyDanny/SwiftLintPlugins.git",
             exact: "0.59.1"),
         .package(
             url: "https://github.com/Quick/Nimble.git",
@@ -127,6 +127,7 @@ package.targets.forEach { target in
         .regular,
         .test,
         .executable,
+        .macro,
     ]
 
     guard types.contains(target.type)
@@ -143,7 +144,7 @@ package.targets.forEach { target in
 
     if enableSwiftLintBuildToolPlugin {
         target.plugins = (target.plugins ?? []) + [
-            .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
+            .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins"),
         ]
     }
 }
